8c85e3c3bd667ad871558f030efea131c94b5a3b
Ksenia Koneva <hello@xeniakoneva.co.uk>
2022-07-23 16:47:30 +0100

    add controllers to list, movie and bookmark

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates_uniqueness_of :title
+  validates :overview, presence: true
+
+  has_many :bookmarks
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'lists#index'
+  resources :list do
+    resources :bookmarks, only: [:index, :new, :create]
+  end
+  resources :bookmarks, only: :destroy
   # Defines the root path route ("/")
   # root "articles#index"
 end
